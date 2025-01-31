557e67f7eab2af080ad905e8c589fc4f0dc4d4d7
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2025-01-31 15:56:23 +0000

    Update job.sh

--- job.sh
+++ job.sh
@@ -14,7 +14,12 @@ set -e
 
 # Load modules
 # https://docs.hpc.shef.ac.uk/en/latest/stanage/software/libs/cudnn.html
-# module load cuDNN
+module load cuDNN
+
+# View CUDA information
+# https://docs.nvidia.com/deploy/nvidia-smi/index.html
+nvidia-smi
+nvcc --version
 
 # Run container
-/usr/local/bin/apptainer test meltingpot.sif
+/usr/local/bin/apptainer test --nv meltingpot.sif
